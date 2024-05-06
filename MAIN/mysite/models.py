e90fa078657611f365e43cd4f752686ad0ebbf69
dima_tolshin <dima_tolshin@mail.ru>
2024-05-06 21:23:57 +0300

    remake

--- MAIN/mysite/models.py
+++ MAIN/mysite/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 class Parent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.TextField(max_length=15, blank=True, null=True)
+    surname = models.TextField(max_length=15, blank=True, null=True)
+    mobile_number = models.IntegerField(default=0, blank=True, null=True)
     photo = models.ImageField(default='Helping-Project/MAIN/photo/default.jpg', upload_to='photo')
     birth_date = models.DateField(blank=True, auto_now=False, auto_now_add=False, null=True)
 
@@ -12,11 +15,20 @@ class Parent(models.Model):
         return f'{self.user.username}'
 
 
+class InviteStatus(models.TextChoices):
+    DEFAULT = 'DEFAULT',
+    SUCCESS = 'SUCCESS'
+
+
 class Children(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.TextField(max_length=15, blank=True, null=True)
+    surname = models.TextField(max_length=15, blank=True, null=True)
+    mobile_number = models.IntegerField(default=0, blank=True, null=True)
     photo = models.ImageField(default='Helping-Project/MAIN/photo/default.jpg', upload_to='photo')
     birth_date = models.DateField(blank=True, auto_now=False, auto_now_add=False, null=True)
-    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='childrens')
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='childrens', blank=True, null=True)
+    status = models.CharField(max_length=10, choices=InviteStatus.choices, default=InviteStatus.DEFAULT)
 
     def __str__(self):
         return f'{self.user.username}'
