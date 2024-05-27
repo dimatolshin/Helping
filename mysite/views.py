2376b2024687c095cb701568aab93d3dfbb5ea2d
TanyaBogdanec <bogdanets_88@mail.ru>
2024-05-27 13:55:47 +0300

    добавлены изменения Димы. переписаны пути и вьюхи

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from rest_framework import permissions, generics
+from rest_framework import permissions, generics, status
 from rest_framework import renderers
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
@@ -8,36 +8,54 @@ from rest_framework import viewsets
 from .models import *
 from .permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 from .serializers import ProfileSearializer, UserSerializer
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
+
+from .services import all_objects
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = User.objects.all()
+class UserList(generics.ListAPIView):
+    queryset = all_objects(User)
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+class UserCreate(generics.CreateAPIView):
+    queryset = all_objects(User)
     serializer_class = UserSerializer
 
+class UserUpdate(generics.RetrieveUpdateAPIView):
+    queryset = all_objects(User)
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class UserDestroy(generics.RetrieveDestroyAPIView):
+    queryset = all_objects(User)
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class ProfileUpdate(generics.RetrieveUpdateAPIView):
-    queryset = Profile.objects.all()
+    queryset = all_objects(Profile)
     serializer_class = ProfileSearializer
-    permission_classes = (IsOwnerOrReadOnly,IsAdminUser)
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
 class ProfileDestroy(generics.RetrieveDestroyAPIView):
-    queryset = Profile.objects.all()
+    queryset = all_objects(Profile)
     serializer_class = ProfileSearializer
     permission_classes = (IsAdminOrReadOnly,)
 
 
-class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
+class ProfileViewSet(generics.ListCreateAPIView):
+    queryset = all_objects(Profile)
     serializer_class = ProfileSearializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
 
-    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    # @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
 
 
 def index(request):
