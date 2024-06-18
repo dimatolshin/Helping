8aa9919e5c83ae441234ed43276d0817ee16e76f
dima_tolshin <dima_tolshin@mail.ru>
2024-06-19 01:15:40 +0300

    remake

--- mysite/services.py
+++ mysite/services.py
@@ -1,8 +1,13 @@
 "Для того чтоб загружались модели "
 import os
 import django
-import json
 from rest_framework.authtoken.models import Token
+import io
+import json
+import time
+import base64
+import requests
+from PIL import Image
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Project.settings')
 django.setup()
@@ -31,3 +36,63 @@ def set_token_cookie(response, user):
         samesite='Lax'  # Предотвращение отправки куки при запросах с других сайтов
     )
     return response
+
+#Генератор картинок
+class Text2ImageAPI:
+
+    def __init__(self, url, api_key, secret_key):
+        self.URL = url
+        self.AUTH_HEADERS = {
+            'X-Key': f'Key {api_key}',
+            'X-Secret': f'Secret {secret_key}',
+        }
+
+    def get_model(self):
+        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+        data = response.json()
+        return data[0]['id']
+
+    def generate(self, prompt, model, images=1, width=1024, height=1024, style=1):
+        styles = ["KANDINSKY", "UHD", "ANIME", "DEFAULT"]
+        params = {
+            "type": "GENERATE",
+            "numImages": images,
+            "width": width,
+            "style": styles[style],
+            "height": height,
+            "generateParams": {
+                "query": f"{prompt}"
+            }
+        }
+
+        data = {
+            'model_id': (None, model),
+            'params': (None, json.dumps(params), 'application/json')
+        }
+        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        data = response.json()
+        return data['uuid']
+
+    def check_generation(self, request_id, attempts=10, delay=10):
+        while attempts > 0:
+            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            data = response.json()
+            if data['status'] == 'DONE':
+                return data['images']
+
+            attempts -= 1
+            time.sleep(delay)
+
+
+if __name__ == '__main__':
+    api = Text2ImageAPI('https://api-key.fusionbrain.ai/', 'C602B83B68ED45ACAD8F4D67359B4A5C',
+                        'C56D07DE3C885781E5E7592EF7FE92AF')
+    model_id = api.get_model()
+    uuid = api.generate("шарики", model_id, style=0)
+    images = api.check_generation(uuid)
+    image_base64 = images[0]
+    image_data = base64.b64decode(image_base64)
+    # with open("image.jpg", "wb") as file:
+    # file.write(image_data)
+    image = Image.open(io.BytesIO(image_data))
+    image.show()
